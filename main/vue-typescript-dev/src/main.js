534d6ecb38d451ed2117d52d0fb770b3e3d57b13
yuto <60546254+nosscafe66@users.noreply.github.com>
2022-08-11 14:54:37 +0900

    commit5

--- main/vue-typescript-dev/src/main.js
+++ main/vue-typescript-dev/src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// eslint-disable-next-line
+/* eslint-disable */ 
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
 import router from './router'
+import axios from 'axios'  //axiosを使う場合、main.jsでimportする。
 
-createApp(App).use(router).use(router).mount('#app')
+Vue.config.productionTip = false
+Vue.prototype.$axios = axios  //axiosを使う場合、main.jsでこの行の追記が必要
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
